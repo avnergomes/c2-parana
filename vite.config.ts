b43e26807498d45e59828df64ec814b4ae0aeaf8
avnergomes <avnerpaesgomes@gmail.com>
2026-03-17 06:10:05 -0300

    inmet api corrections

--- vite.config.ts
+++ vite.config.ts
@@ -39,7 +39,7 @@ export default defineConfig(({ mode }) => ({
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
         "font-src 'self' https://fonts.gstatic.com",
         "img-src 'self' data: blob: https://*.tile.openstreetmap.org https://firms.modaps.eosdis.nasa.gov",
-        "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://api.stripe.com https://apitempo.inmet.gov.br https://info.dengue.mat.br https://firms.modaps.eosdis.nasa.gov https://api.waqi.info https://servicodados.ibge.gov.br https://precos-diarios-api.onrender.com https://*.ingest.sentry.io",
+        "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://api.stripe.com https://apitempo.inmet.gov.br https://apialerta.inmet.gov.br https://info.dengue.mat.br https://firms.modaps.eosdis.nasa.gov https://api.waqi.info https://servicodados.ibge.gov.br https://precos-diarios-api.onrender.com https://*.ingest.sentry.io",
         "frame-src 'self' https://js.stripe.com",
         "object-src 'none'",
         "base-uri 'self'",
